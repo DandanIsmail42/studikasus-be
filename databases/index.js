dbc5fa278b440bd54cb6062296f1a1ea42b02a27
DandanIsmail42 <dandanismail42@gmail.com>
2022-10-15 23:02:49 +0700

    first comit

--- databases/index.js
+++ databases/index.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
+const database = 
+        process.env.MONGO_URI || `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`;
+
 const {dbHost, dbPass, dbName, dbPort, dbUser} = require('../app/config');
 
-mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
+mongoose.connect(database, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
 const db = mongoose.connection;
 
-
+// mongodb+srv://dandan:<password>@be.d2u423b.mongodb.net/?retryWrites=true&w=majority
 
 module.exports = db;
