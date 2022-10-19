99e7a0cca8a7c7fa460dc27c46c58b13296bf9a8
DandanIsmail42 <dandanismail42@gmail.com>
2022-10-19 22:47:35 +0700

    modify db

--- databases/index.js
+++ databases/index.js
@@ -1,17 +1,38 @@
 const mongoose = require('mongoose');
 const {dbHost, dbPass, dbName, dbPort, dbUser} = require('../app/config');
 
-const database = 
-        process.env.MONGO_URI || 'mongodb+srv://dandan:FwZUybsycYVk2BL0@be.d2u423b.mongodb.net/foodstore?retryWrites=true&w=majority' || `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`; 
 
-
-
-mongoose.connect(database, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-});
-const db = mongoose.connection;
-
-// mongodb+srv://dandan:<password>@be.d2u423b.mongodb.net/?retryWrites=true&w=majority
+const db = async () => {
+    await mongoose.connect(
+        'mongodb+srv://dandan:FwZUybsycYVk2BL0@be.d2u423b.mongodb.net/foodstore?retryWrites=true&w=majority'
+    );
+};
 
 module.exports = db;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const database = 
+//         process.env.MONGO_URI || 'mongodb+srv://dandan:FwZUybsycYVk2BL0@be.d2u423b.mongodb.net/foodstore?retryWrites=true&w=majority' || `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`; 
+
+
+
+// mongoose.connect(database, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+// });
+// const db = mongoose.connection;
+
+// // mongodb+srv://dandan:<password>@be.d2u423b.mongodb.net/?retryWrites=true&w=majority
+
+// module.exports = db;
