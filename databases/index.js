b3249295898cb9886adf65cea7a9738dc4f38413
DandanIsmail42 <dandanismail42@gmail.com>
2022-10-20 19:07:08 +0700

    modify db2

--- databases/index.js
+++ databases/index.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
 const {dbHost, dbPass, dbName, dbPort, dbUser} = require('../app/config');
 
-const database = 
+const database = async () => {
         process.env.MONGO_URI || `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`;
 
-
-
-mongoose.connect(database, {
+await mongoose.connect(database, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
 });
+}
 const db = mongoose.connection;
 
 // mongodb+srv://dandan:<password>@be.d2u423b.mongodb.net/?retryWrites=true&w=majority
