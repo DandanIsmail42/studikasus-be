6662c58d312c88ee434347cb608af3a8e155d352
DandanIsmail42 <dandanismail42@gmail.com>
2022-10-25 23:46:35 +0700

    modif appjs

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
   next();
 })
 app.use(logger('dev'));
