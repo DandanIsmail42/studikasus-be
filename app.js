b0b1af8187d7e6b61e4c5a7d428bd4f100904955
DandanIsmail42 <dandanismail42@gmail.com>
2022-10-25 20:50:07 +0700

    add url fe at cors

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 
-app.use(cors());
+app.use(cors('https://foodstore-application-dandan.herokuapp'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
