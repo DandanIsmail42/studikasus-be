5d6e23ed3974da7b3050fbbf0cb8e3458ba24774
DandanIsmail42 <dandanismail42@gmail.com>
2022-10-26 16:31:43 +0700

    modif appjs3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// const cors = require('cors');
+const cors = require('cors');
 const {decodeToken} = require('./middlewares')
 const productRoute = require('./app/product/routes');
 const categoryRoute = require('./app/category/routes');
@@ -22,13 +22,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-
-// app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
   next();
+app.use(cors());
+
 })
 app.use(logger('dev'));
 app.use(express.json());
